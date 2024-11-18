9e14877d2e49eba33e70ab0771532d6ef0e67617
Magnus-Hansen <54136904+Magnus-Hansen@users.noreply.github.com>
2024-11-18 13:31:45 +0100

    GetAllTest

--- DRPladesamlingREST2Tests/Collections/RecordsRepositoryListTests.cs
+++ DRPladesamlingREST2Tests/Collections/RecordsRepositoryListTests.cs
@@ -10,13 +10,22 @@ namespace DRPladesamlingREST2.Collections.Tests
 {
     [TestClass()]
     public class RecordsRepositoryListTests
-    {
-      
+    {  
+        private RecordsRepositoryList _recordsRepo;
+
+        [TestInitialize()]
+        public void SetUp()
+        {
+            _recordsRepo = new RecordsRepositoryList();
+        }
 
         [TestMethod()]
         public void GetAllTest()
         {
-            Assert.Fail();
+            Assert.AreEqual(0, _recordsRepo.GetAll().Count);
+            Record record = new Record("Title", "Artist", 55, 2001);
+            _recordsRepo.Add(record);
+            Assert.AreEqual(1, _recordsRepo.GetAll().Count);
         }
     }
 }
