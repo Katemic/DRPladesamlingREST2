051eb80e07df23e38cf8a2aa9f4bc7f8fdd39e38
Katrine <kah003@edu.zealand.dk>
2024-11-18 12:40:32 +0100

    added model class

--- DRPladesamlingREST2/Collections/Record.cs
+++ DRPladesamlingREST2/Collections/Record.cs
@@ -1,6 +1,43 @@
-﻿namespace DRPladesamlingREST2.Collections
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DRPladesamlingREST2.Collections
 {
     public class Record
     {
+        //A music record has attributes like
+        //title, artist, duration, publicationYear (dates not allowed: dates = problems when you program)
+        //and maybe more attributes …Duration is the of number seconds(a simple int)
+
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public int PublicationYear { get; set; }
+
+
+        public Record(string title, string artist, int duration, int publicationYear)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            PublicationYear = publicationYear;
+            
+        }
+
+        public Record()
+        {
+            
+        }
+
+
+        
+
+
+        public void Validate()
+        {
+
+        }
+
+
+
     }
 }
